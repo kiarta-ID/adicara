d5bed88307999b2c8b3ad8e726c590d59bdf22d3
MSerperior <45038103+MSerperior@users.noreply.github.com>
2022-04-26 14:02:50 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::resource('user', UserController::class);
 
-    Route::resource('event', EventController    ::class);
+    Route::resource('event', EventController::class);
     
     // Route::get('/documents', function (Request $request) {
     //     return new DocumentCollection(Document::all());
